3453feed150f0ccb5c4093bcc2854f00dfc76139
aismeg <0>
2023-02-23 20:24:51 +0300

    navbar ok

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const navbar = document.querySelector('.navbar');
+const headerLogo = navbar.querySelector('.header-logo');
+const headerNav = navbar.querySelector('.header-nav');
+const logoDark = navbar.querySelector('.logo-light');
+const phoneLink = navbar.querySelector('.header-phone');
+
+document.addEventListener('scroll', () => {
+    let current = window.scrollY;
+
+    if (current >= 150) {
+        navbar.style.backgroundColor = 'white';
+        headerLogo.style.display = 'flex';
+        headerLogo.style.alignItems = 'center';
+        navbar.style.height = '72px';
+        headerNav.style.color = '#292D33';
+        logoDark.src = './img/logo-dark.svg';
+        phoneLink.style.color = '#292D33';
+    } else {
+        navbar.style.backgroundColor = '';
+        navbar.style.height = '';
+        headerLogo.style.display = '';
+        headerLogo.style.alignItems = '';
+        headerNav.style.color = '';
+        logoDark.src = './img/logo-light.svg';
+        phoneLink.style.color = '';
+    }
+});

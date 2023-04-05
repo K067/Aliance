8da5239177459fd73a09205b2428709f88969820
aismeg <0>
2023-04-05 18:20:23 +0300

    contract

--- contract.php
+++ contract.php
@@ -1,7 +1,131 @@
 <?php 
 $page_title='Контрактное производство';
 $header_style='bg-grey';
-include_once('header.php');
-include_once('./templates/contract.php');
-?>
+include_once('header.php');?>
+<section class="section production mg-alt">
+        <div class="container">
+            <div class="cards">
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Автомобильная химия</h3>
+                        <p class="card-text">Безусловно, сплочённость команды профессионалов позволяет оценить значение
+                            форм
+                            воздействия.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/avto-him.webp">
+                    <source type="image/png" srcset="./img/avto-him.png">
+                    <img src="./img/avto-him.png" alt="Автомобильная химия" class="image-scale">
+                    </picture>
+                </a>
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Бытовая химия</h3>
+                        <p class="card-text">А также стремящиеся вытеснить традиционное производство, нанотехнологии
+                            функционально разнесены на независимые элементы.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/bit-him.webp">
+                    <source type="image/png" srcset="./img/bit-him.png">
+                    <img src="./img/bit-him.png" alt="Бытовая химия" class="image-scale">
+                    </picture>
+                </a>
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Дезинфицирующие средства</h3>
+                        <p class="card-text">Лишь интерактивные прототипы призваны к ответу.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/dezinfect.webp">
+                    <source type="image/png" srcset="./img/dezinfect.png">
+                    <img src="./img/dezinfect.png" alt="Дезинфицирующие средства" class="image-scale">
+                    </picture>
+                </a>
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Пищевые аэрозоли</h3>
+                        <p class="card-text">Безусловно, сплочённость команды профессионалов позволяет оценить значение
+                            форм
+                            воздействия.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/dezinfect-1.webp">
+                    <source type="image/png" srcset="./img/dezinfect-1.png">
+                    <img src="./img/dezinfect-1.png" alt="Пищевые аэрозоли" class="image-scale">
+                    </picture>
+                </a>
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Косметическая продукция</h3>
+                        <p class="card-text">Лишь интерактивные прототипы призваны к ответу.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/avto-him-1.webp">
+                    <source type="image/png" srcset="./img/avto-him-1.png">
+                    <img src="./img/avto-him-1.png" alt="Косметическая продукция" class="image-scale">
+                    </picture>
+                </a>
+                <a href="#" class="card">
+                    <div class="card-content">
+                        <h3 class="card-title">Краски аэрозольные</h3>
+                        <p class="card-text">А также стремящиеся вытеснить традиционное производство, нанотехнологии
+                            функционально разнесены на независимые элементы.</p>
+                    </div>
+                    <picture class="card-image">
+                    <source type="image/webp" srcset="./img/bit-him-1.webp">
+                    <source type="image/png" srcset="./img/bit-him-1.png">
+                    <img src="./img/bit-him-1.png" alt="Краски аэрозольные" class="image-scale">
+                    </picture>
+                </a>
+            </div>
+        </div>
+    </section>
+
+<section class="section seo">
+    <div class="container">
+        <div class="seo-wrapper">
+            <h3 class="seo-title">SEO Заголовок</h3>
+            <p class="seo-text">Также как дальнейшее развитие различных форм деятельности предполагает независимые способы реализации существующих финансовых и административных условий. Каждый из нас понимает очевидную вещь: курс на социально-ориентированный национальный проект играет важную роль в формировании как самодостаточных, так и внешне зависимых концептуальных решений. Внезапно, интерактивные прототипы набирают популярность среди определенных слоев населения, а значит, должны быть...</p>
+        </div>
+            <a href="" class="button-fold">Читать больше</a>
+    </div>
+</section>
+
+<section class="cta cta-alt">
+        <div class="bg-grey section-cta research-section">
+        <picture class="cta-image">
+        <source type="image/webp" srcset="./img/__400-1.webp">
+        <source type="image/png" srcset="./img/__400-1.png">
+        <img src="./img/__400-1.png" alt="cta" class="image-scale">
+        </picture>
+            <div class="cta-form-wrapper container">
+                <form action="./handler.php" class="cta-form">
+                    <h2 class="section-title cta-form-title">Хотите сотрудничать?</h2>
+                    <p class="cta-form-text">Оставьте заявку, наш менеджер свяжется с Вами в ближайшее время ответит
+                        на все
+                        интересующие вопросы и поможем даже в самых сложных случаях!</p>
+                    <div class="input-group-wrapper">
+                        <div class="input-group">
+                            <input id="user-name" type="text" class="input" name="username" placeholder=" ">
+                            <label for="user-name" class="label">Имя</label>
+                        </div>
+                        <div class="input-group">
+                            <input id="user-phone" type="tel" class="input" name="userphone" placeholder=" ">
+                            <label for="user-phone" class="label">Номер телефона</label>
+                        </div>
+                    </div>
+                    <div class="cta-form-footer">
+                        <button type="submit" class="button cta-form-button send-it">Отправить заявку</button>
+                        <div class="notify">
+                            <img src="./img/shield.svg" alt="form icon" class="notify-icon">
+                            <p class="notify-text">
+                                Обращаясь к нам вы получаете не только профессиональную работу, но и абсолютную
+                                конфиденциальность информации!
+                            </p>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+</section>
 <?php include_once('footer.php');?>

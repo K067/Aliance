36ed199093a2a911dc32891e3abc3e14520541a7
aismeg <0>
2023-04-05 05:03:32 +0300

    about done

--- about.php
+++ about.php
@@ -98,9 +98,41 @@ include_once('header.php');?>
             </div>
         </div>
     </section>
-<?php include_once('./templates/founder.php');?>
-<section class="section blog-research research-section">
+    <section class="section founder">
+<picture class="founder-image">
+  <source type="image/webp" srcset="./img/pexels-tiger-lily-44839381.webp">
+  <source type="image/png" srcset="./img/pexels-tiger-lily-44839381.png">
+  <img src="./img/pexels-tiger-lily-44839381.png" alt="founder" class="image-scale">
+</picture>
         <div class="container">
+            <div class="founder-content-wrapper">
+                <div class="founder-content">
+                    <div class="separator"></div>
+                    <h2 class="section-title founder-title">Отношение к делу и к клиентам</h2>
+                    <p class="founder-text">Кстати, интерактивные прототипы описаны максимально подробно. Повседневная
+                        практика показывает, что укрепление и развитие внутренней структуры говорит о возможностях
+                        соответствующих условий активизации. Внезапно, независимые государства, которые представляют
+                        собой
+                        яркий пример континентально-европейского типа политической культуры, будут подвергнуты целой
+                        серии
+                        независимых исследований. С учётом сложившейся международной обстановки, синтетическое
+                        тестирование
+                        выявляет срочную потребность системы массового участия.
+                    </p>
+                    <p class="founder-text text-beta">
+                        А ещё действия представителей оппозиции, превозмогая сложившуюся непростую экономическую
+                        ситуацию, в
+                        равной степени предоставлены сами себе. Не следует, однако, забывать, что выбранный нами
+                        инновационный путь в значительной степени обусловливает важность дальнейших направлений
+                        развития.
+                    </p>
+                    <a href="./about.php" class="button-link mission">О нашей миссии</a>
+                </div>
+            </div>
+        </div>
+    </section>
+<section class="section blog-research research-section">
+        <div class="container research-type">
             <div class="separator"></div>
             <h2 class="section-title research-title">Исследовательский центр Aliance production</h2>
             <div class="text-research-content">
--- index.php
+++ index.php
@@ -16,7 +16,7 @@
     <div class="mobile-menu">
         <ul class="mobile-menu-nav">
             <li class="mobile-menu-nav-item">
-                <a href="" class="mobile-menu-link">О компании</a>
+                <a href="./about.php" class="mobile-menu-link">О компании</a>
             </li>
             <li class="mobile-menu-nav-item">
                 <a href="#" class="mobile-menu-link">Контрактное производство</a>
@@ -144,7 +144,39 @@
  <?php include_once('./templates/schem.php')?>
  <?php $block_title="Контрактное производство"; include_once('./templates/contract.php')?>
  <?php $block_title="Собственные торговые марки"; include_once('./templates/tradebrand.php')?>
- <?php include_once('./templates/founder.php')?>
+ <section class="section founder">
+<picture class="founder-image">
+  <source type="image/webp" srcset="./img/pexels-tiger-lily-44839381.webp">
+  <source type="image/png" srcset="./img/pexels-tiger-lily-44839381.png">
+  <img src="./img/pexels-tiger-lily-44839381.png" alt="founder" class="image-scale">
+</picture>
+        <div class="container">
+            <div class="founder-content-wrapper">
+                <div class="founder-content">
+                    <div class="separator"></div>
+                    <h2 class="section-title founder-title">Отношение к делу и к клиентам</h2>
+                    <p class="founder-text">Кстати, интерактивные прототипы описаны максимально подробно. Повседневная
+                        практика показывает, что укрепление и развитие внутренней структуры говорит о возможностях
+                        соответствующих условий активизации. Внезапно, независимые государства, которые представляют
+                        собой
+                        яркий пример континентально-европейского типа политической культуры, будут подвергнуты целой
+                        серии
+                        независимых исследований. С учётом сложившейся международной обстановки, синтетическое
+                        тестирование
+                        выявляет срочную потребность системы массового участия.
+                    </p>
+                    <p class="founder-text text-beta">
+                        А ещё действия представителей оппозиции, превозмогая сложившуюся непростую экономическую
+                        ситуацию, в
+                        равной степени предоставлены сами себе. Не следует, однако, забывать, что выбранный нами
+                        инновационный путь в значительной степени обусловливает важность дальнейших направлений
+                        развития.
+                    </p>
+                    <a href="./about.php" class="button-link">Подробнее о компании</a>
+                </div>
+            </div>
+        </div>
+    </section>
  <?php include_once('./templates/clients.php')?>
     <section class="section blog">
         <div class="container">
